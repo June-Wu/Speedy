4456c293c7afe8433194b7e1dda8e217e74d38de
June <june.wu.cs@gmail.com>
2020-05-01 23:00:43 -0700

    left right movement control

---
+++ Speedy Game/Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 3000f;
+    public float xForce = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate() 
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        xControl();
+    }
+
+    private void xControl()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(xForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(-xForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
