ecd2d2f05ba44eb5dd054dc9e304e013342b5eae
June <june.wu.cs@gmail.com>
2020-05-03 22:45:56 -0700

    camera following

--- Speedy Game/Assets/Scripts/PlayerMovements.cs
+++ Speedy Game/Assets/Scripts/PlayerMovements.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PlayerMovements : MonoBehaviour
 {
     public Rigidbody rb;
+    public Vector3 startingPos;
     public float forwardForce = 3000f;
     public float xForce = 1000f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        transform.position = startingPos;
     }
 
     // Update is called once per frame
@@ -24,11 +26,11 @@ public class PlayerMovements : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rb.AddForce(xForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-xForce * Time.deltaTime, 0, 0);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            rb.AddForce(-xForce * Time.deltaTime, 0, 0);
+            rb.AddForce(xForce * Time.deltaTime, 0, 0);
         }
     }
 }
